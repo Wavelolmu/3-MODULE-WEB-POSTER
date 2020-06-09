9823ae7702618ad54368b282022937d238d49919
Vitaly Gachkovsky <wavelolmu@gmail.com>
2020-06-09 14:48:46 +0300

    WIP Первая секция
    Написал анимацию руки с губкой

--- js/main.js
+++ js/main.js
@@ -11,3 +11,20 @@ function starterDisappear() {
 };
 
 setTimeout(starterDisappear, 2000);
+
+function initializeHand(){
+  var handHeight = String(getComputedStyle(document.documentElement).getPropertyValue('--hand-height')).slice(0, 4);
+  spaceH = screen.height - handHeight - 50;
+  spaceW = screen.width - (handHeight / 1.5) - 50;
+  setInterval(moveHand1, 700);
+  console.log(handHeight);
+}
+
+function moveHand1(handHeight) {
+  document.querySelector('#hand1').style.top = Math.round(Math.random() * spaceH) + 'px';
+  document.querySelector('#hand1').style.left = Math.round(Math.random() * spaceW) + 'px';
+  var rotateAngle  = Math.round(Math.random() * 120 - Math.random() * 120) + 'deg';
+  document.querySelector('#hand1').style.transform = `rotate(${rotateAngle})`;
+}
+
+// window.onload = initializeHand();
